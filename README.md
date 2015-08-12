585ac0ff8ad04c72feec2eb64fbf6f476928612e
grg2rsr <grg2rsr@gmail.com>
2015-08-12 20:40:44 +0200

    starting OO

--- README.md
+++ README.md
@@ -3,5 +3,5 @@ Python code to visualize geometrical patterns produced by poi.
 ![](https://github.com/grg2rsr/Poi_visualization/blob/master/Screenshot.png) 
 ### dependencies
 + scipy
-+ pyqtgraph with opengl
-
++ pyqtgraph
++ python openGL bindings
--- poi_code_recode.py
+++ poi_code_recode.py
@@ -32,7 +32,7 @@ tvec = sp.linspace(0,2*sp.pi,tsteps)
 
 ### constants
 shoulder_width = 0.4
-string_length = 1
+string_length = 1.2
 arm_length = 1
 
 # offset to ground level plane, but center of view rotation doesn't change
@@ -68,8 +68,8 @@ poi denotes the total poi position (poi + hand)
 #pattern_right = sp.array([[ 1,-3, 0], [ 1,-1, 0], [ 0, 1, 0]])
 
 ## classic antispin flower
-pattern_left  = sp.array([[ 0.8, 3, 0], [ 1,-1, 0], [ 0, 1, 0]])
-pattern_right = sp.array([[ 0.8,-3, 0], [ 1, 1, 0], [ 0, 1, 0]])
+pattern_left  = sp.array([[ string_length, 3, 0], [ arm_length,-1, 0], [ 0, 1, 0]])
+pattern_right = sp.array([[ string_length,-3, 0], [ arm_length, 1, 0], [ 0, 1, 0]])
 
 Pattern = sp.concatenate((pattern_left[:,:,sp.newaxis],pattern_right[:,:,sp.newaxis]),axis=2)
 
@@ -101,11 +101,40 @@ Pos[:,1,:,1] += shoulder_width/2
 #==============================================================================
 # visualization setup
 #==============================================================================
+
+class myGLLine(gl.GLLinePlotItem):
+    """ subclassing GLLine """
+    def __init__(self,PosMat=None,side='right',**kwargs):
+        gl.GLLinePlotItem.__init__(self,**kwargs)
+        self.side = side
+        self.PosMat = PosMat # tvec.shape[0] x 3 x 2 , last dimension is the fixed point
+    
+    def update_(self,i):
+        """ updates position based on i """
+        self.setData(pos=self.PosMat[i,:,:].T) # receices 2 x 3 as from, to
     
     
+class myGLScatter(gl.GLScatterPlotItem):
+    """ subclassing GLScatter """
+    def __init__(self,pos,side='right',**kwargs):
+        gl.GLScatterPlotItem.__init__(self,**kwargs)
+        self.side = side
+        self.pos = pos # tvec.shape[0] x 3
+#        self.update_(0)
+    
+    def update_(self,i):
+        """ updates position based on i """
+        self.setData(self.pos[i,:])
+        
+#poi_left_scatter = gl.GLScatterPlotItem(pos=poi_left_pos,color=poi_left_col,size=0.2,pxMode=False)
+
+
 """ also cool idea to implement: a slider that sets the trace position from a 
 fraction of the string length """
 
+
+""" rename variables:
+poi, hand, trace, line"""
 ### colors
 poi_left_col = (0.2,1,0.5,0.9) # replace by color from qt based color picking widget
 hand_left_col = (0.2,0.5,1,0.9)
@@ -114,14 +143,6 @@ hand_right_col = (1,0.5,0.2,0.9)
 poi_right_col = (1,0.2,0.5,0.9)
 
 
-## left
-#left_side = {}
-#left_side['hands']
-#left_side['strings']
-#left_side['pois']
-#left_side['hand_trace']
-#left_side['poi_trace']
-
 ### taking the pos back out of the array, just for checking, will be replaced alter
 poi_left_pos = Pos[:,:,1,1]
 hand_left_pos = Pos[:,:,2,1]
@@ -129,8 +150,14 @@ hand_left_pos = Pos[:,:,2,1]
 poi_right_pos = Pos[:,:,1,0]
 hand_right_pos = Pos[:,:,2,0]
 
-poi_left_line = gl.GLLinePlotItem(pos=poi_left_pos,color=poi_left_col)
-hand_left_line = gl.GLLinePlotItem(pos=hand_left_pos,color=hand_left_col)
+data_left = sp.concatenate((Pos[:,:,1,1,sp.newaxis],Pos[:,:,2,1,sp.newaxis]),axis=2)
+data_right = sp.concatenate((Pos[:,:,1,0,sp.newaxis],Pos[:,:,2,0,sp.newaxis]),axis=2)
+
+poi_left_string = myGLLine(PosMat = data_left,pos=data_left[1].T,color=poi_left_col)
+poi_right_string = myGLLine(PosMat = data_right,pos=data_left[0].T,color=poi_right_col)
+
+#poi_left_line = gl.GLLinePlotItem(pos=poi_left_pos,color=poi_left_col)
+#hand_left_line = gl.GLLinePlotItem(pos=hand_left_pos,color=hand_left_col)
 
 poi_left_scatter = gl.GLScatterPlotItem(pos=poi_left_pos,color=poi_left_col,size=0.2,pxMode=False)
 hand_left_scatter = gl.GLScatterPlotItem(pos=hand_left_pos,color=hand_left_col,size=0.1,pxMode=False)
@@ -139,31 +166,32 @@ string_left_line = gl.GLLinePlotItem(pos=sp.vstack((hand_left_pos[0],poi_left_po
 #arm_left = gl.GLLinePlotItem(pos=sp.vstack((hand_left_pos[0],[0,-shoulder_width/2,0])),color=poi_left_col,width=2)
 arm_left = gl.GLLinePlotItem(pos=sp.vstack((hand_left_pos[0],[0,0,offset])),color=poi_left_col,width=2)
 
-w.addItem(poi_left_line)
-w.addItem(poi_left_scatter)
-w.addItem(hand_left_line)
-w.addItem(hand_left_scatter)
-w.addItem(arm_left)
-w.addItem(string_left_line)
+w.addItem(poi_left_string)
+w.addItem(poi_right_string)
+#w.addItem(poi_left_scatter)
+#w.addItem(hand_left_line)
+#w.addItem(hand_left_scatter)
+#w.addItem(arm_left)
+#w.addItem(string_left_line)
 
 ## right
-poi_right_line = gl.GLLinePlotItem(pos=poi_right_pos,color=poi_right_col)
-hand_right_line = gl.GLLinePlotItem(pos=hand_right_pos,color=hand_right_col)
-poi_right_scatter = gl.GLScatterPlotItem(pos=poi_right_pos,color=poi_right_col,size=0.2,pxMode=False)
-hand_right_scatter = gl.GLScatterPlotItem(pos=hand_right_pos,color=hand_right_col,size=0.1,pxMode=False)
-string_right_line = gl.GLLinePlotItem(pos=sp.vstack((hand_right_pos[0],poi_right_pos[0])),color=poi_right_col,width=2)
-#arm_right = gl.GLLinePlotItem(pos=sp.vstack((hand_right_pos[0],[0,shoulder_width/2,0])),color=poi_right_col,width=2)
-arm_right = gl.GLLinePlotItem(pos=sp.vstack((hand_right_pos[0],[0,0,offset])),color=poi_right_col,width=2)
-
-w.addItem(poi_right_line)
-w.addItem(poi_right_scatter)
-w.addItem(hand_right_line)
-w.addItem(hand_right_scatter)
-w.addItem(arm_right)
-w.addItem(string_right_line)
+#poi_right_line = gl.GLLinePlotItem(pos=poi_right_pos,color=poi_right_col)
+#hand_right_line = gl.GLLinePlotItem(pos=hand_right_pos,color=hand_right_col)
+#poi_right_scatter = gl.GLScatterPlotItem(pos=poi_right_pos,color=poi_right_col,size=0.2,pxMode=False)
+#hand_right_scatter = gl.GLScatterPlotItem(pos=hand_right_pos,color=hand_right_col,size=0.1,pxMode=False)
+#string_right_line = gl.GLLinePlotItem(pos=sp.vstack((hand_right_pos[0],poi_right_pos[0])),color=poi_right_col,width=2)
+##arm_right = gl.GLLinePlotItem(pos=sp.vstack((hand_right_pos[0],[0,shoulder_width/2,0])),color=poi_right_col,width=2)
+#arm_right = gl.GLLinePlotItem(pos=sp.vstack((hand_right_pos[0],[0,0,offset])),color=poi_right_col,width=2)
+#
+#w.addItem(poi_right_line)
+#w.addItem(poi_right_scatter)
+#w.addItem(hand_right_line)
+#w.addItem(hand_right_scatter)
+#w.addItem(arm_right)
+#w.addItem(string_right_line)
 
 #w.setCameraPosition(elevation=3*offset,distance=10)
-
+items = w.items[1:]
 #==============================================================================
 # Animation run
 #==============================================================================
@@ -182,34 +210,36 @@ def update():
     GUI interaction: iterate over all visible objects and call an update function
     each timestep. update will include new position and show/not show, color etc.
     maybe subclassing necessary"""
+    for item in items:
+        item.update_(i)
+        
+#    if show_left:
+#        poi_left_scatter.setData(pos=sp.expand_dims(poi_left_pos[i],0))
+#        hand_left_scatter.setData(pos=sp.expand_dims(hand_left_pos[i],0))
+#        string_left_line.setData(pos=sp.vstack((hand_left_pos[i],poi_left_pos[i])))
+##        arm_left.setData(pos=sp.vstack((hand_left_pos[i],[0,-1*shoulder_width/2,0])))
+#        arm_left.setData(pos=sp.vstack((hand_left_pos[i],[0,0,offset])))
+#    else:
+#        poi_left_scatter.hide()
+#        poi_left_line.hide()
+#        hand_left_scatter.hide()
+#        hand_left_line.hide()
+#        string_left_line.hide()
+#        arm_left.hide()
     
-    if show_left:
-        poi_left_scatter.setData(pos=sp.expand_dims(poi_left_pos[i],0))
-        hand_left_scatter.setData(pos=sp.expand_dims(hand_left_pos[i],0))
-        string_left_line.setData(pos=sp.vstack((hand_left_pos[i],poi_left_pos[i])))
-#        arm_left.setData(pos=sp.vstack((hand_left_pos[i],[0,-1*shoulder_width/2,0])))
-        arm_left.setData(pos=sp.vstack((hand_left_pos[i],[0,0,offset])))
-    else:
-        poi_left_scatter.hide()
-        poi_left_line.hide()
-        hand_left_scatter.hide()
-        hand_left_line.hide()
-        string_left_line.hide()
-        arm_left.hide()
-    
-    if show_right:
-        poi_right_scatter.setData(pos=sp.expand_dims(poi_right_pos[i],0))
-        hand_right_scatter.setData(pos=sp.expand_dims(hand_right_pos[i],0))
-        string_right_line.setData(pos=sp.vstack((hand_right_pos[i],poi_right_pos[i])))
-#        arm_right.setData(pos=sp.vstack((hand_right_pos[i],[0,shoulder_width/2,0])))
-        arm_right.setData(pos=sp.vstack((hand_right_pos[i],[0,0,offset])))
-    else:
-        poi_right_scatter.hide()
-        poi_right_line.hide()
-        hand_right_scatter.hide()
-        hand_right_line.hide()
-        string_right_line.hide()
-        arm_right.hide()
+#    if show_right:
+#        poi_right_scatter.setData(pos=sp.expand_dims(poi_right_pos[i],0))
+#        hand_right_scatter.setData(pos=sp.expand_dims(hand_right_pos[i],0))
+#        string_right_line.setData(pos=sp.vstack((hand_right_pos[i],poi_right_pos[i])))
+##        arm_right.setData(pos=sp.vstack((hand_right_pos[i],[0,shoulder_width/2,0])))
+#        arm_right.setData(pos=sp.vstack((hand_right_pos[i],[0,0,offset])))
+#    else:
+#        poi_right_scatter.hide()
+#        poi_right_line.hide()
+#        hand_right_scatter.hide()
+#        hand_right_line.hide()
+#        string_right_line.hide()
+#        arm_right.hide()
 
 """ replace with a OO structure, MainWindow etc. put pg Widget into a
 Qt.MainWindow, next to it vis and pattern controls """
